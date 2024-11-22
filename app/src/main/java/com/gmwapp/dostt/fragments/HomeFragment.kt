cecedd347f3777ad7c0100a82dc570e7974b7746
Sivavishnu0705 <32070669+Sivavishnu0705@users.noreply.github.com>
2024-11-22 09:50:44 +0530

    updated

--- app/src/main/java/com/gmwapp/dostt/fragments/HomeFragment.kt
+++ app/src/main/java/com/gmwapp/dostt/fragments/HomeFragment.kt
@@ -23,7 +23,9 @@ class HomeFragment : BaseFragment() {
     }
 
     fun initFab(){
-        binding.fabRandom.shrink()
+        binding.fabRandom.extend()
+        binding.fabAudio.hide()
+        binding.fabVideo.hide()
         binding.fabRandom.setOnClickListener {
             if (!isAllFabVisible) {
                 binding.fabAudio.show()
@@ -31,7 +33,7 @@ class HomeFragment : BaseFragment() {
                 binding.tvAudio.setVisibility(View.VISIBLE)
                 binding.tvVideo.setVisibility(View.VISIBLE)
 
-                binding.fabRandom.extend()
+                binding.fabRandom.shrink()
                 isAllFabVisible = true
             } else {
                 binding.fabAudio.hide()
@@ -39,7 +41,7 @@ class HomeFragment : BaseFragment() {
                 binding.tvAudio.setVisibility(View.GONE)
                 binding.tvVideo.setVisibility(View.GONE)
 
-                binding.fabRandom.shrink()
+                binding.fabRandom.extend()
 
                 isAllFabVisible = false
             }
