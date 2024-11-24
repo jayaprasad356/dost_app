0799e9dfaa0922d3d59bc104c2b3d5c33b0c8543
Sivavishnu0705 <32070669+Sivavishnu0705@users.noreply.github.com>
2024-11-24 15:04:17 +0530

    updated

--- app/src/main/java/com/gmwapp/dostt/activities/LoginActivity.kt
+++ app/src/main/java/com/gmwapp/dostt/activities/LoginActivity.kt
@@ -1,9 +1,14 @@
 package com.gmwapp.dostt.activities
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
+import android.view.MotionEvent
 import android.view.View
-import android.view.WindowManager
+import android.view.View.OnTouchListener
 import com.gmwapp.dostt.R
 import com.gmwapp.dostt.constants.DConstants
 import com.gmwapp.dostt.databinding.ActivityLoginBinding
@@ -15,6 +20,11 @@ class LoginActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun initUI(){
         binding.cvLogin.setBackgroundResource(R.drawable.card_view_border);
 
         binding.btnLogin.setOnClickListener(View.OnClickListener {
@@ -22,6 +32,26 @@ class LoginActivity : BaseActivity() {
             intent.putExtra(DConstants.MOBILE_NUMBER, binding.etMobileNumber.text.toString())
             startActivity(intent)
 
+        })
+        binding.etMobileNumber.setOnTouchListener(OnTouchListener { v, event ->
+            binding.cvLogin.setBackgroundResource(R.drawable.card_view_border_active)
+            false
+        })
+        binding.etMobileNumber.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                window.statusBarColor = resources.getColor(R.color.dark_blue);
+                if(TextUtils.isEmpty(s)){
+                    binding.btnLogin.setBackgroundResource(R.drawable.d_button_bg)
+                }else{
+                    binding.btnLogin.setBackgroundResource(R.drawable.d_button_bg_white)
+                }
+            }
+
+            override fun afterTextChanged(s: Editable) {
+            }
         })
     }
 }
