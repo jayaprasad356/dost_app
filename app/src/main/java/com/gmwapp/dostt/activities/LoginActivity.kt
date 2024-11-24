27b2500f514eb4eca709802554dd5b7ce6cd22dd
Sivavishnu0705 <32070669+Sivavishnu0705@users.noreply.github.com>
2024-11-24 14:34:33 +0530

    updated

--- app/src/main/java/com/gmwapp/dostt/activities/LoginActivity.kt
+++ app/src/main/java/com/gmwapp/dostt/activities/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.gmwapp.dostt.activities
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import com.gmwapp.dostt.R
 import com.gmwapp.dostt.constants.DConstants
 import com.gmwapp.dostt.databinding.ActivityLoginBinding
@@ -15,12 +16,7 @@ class LoginActivity : BaseActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.cvLogin.setBackgroundResource(R.drawable.card_view_border);
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+
         binding.btnLogin.setOnClickListener(View.OnClickListener {
             val intent = Intent(this, VerifyOTPActivity::class.java)
             intent.putExtra(DConstants.MOBILE_NUMBER, binding.etMobileNumber.text.toString())
