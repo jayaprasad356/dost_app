1991643e18508a61dd14d62c6e7a8caee95e0c2e
Sivavishnu0705 <32070669+Sivavishnu0705@users.noreply.github.com>
2024-11-28 12:04:37 +0530

    Update BaseActivity.kt

--- app/src/main/java/com/gmwapp/dostt/activities/BaseActivity.kt
+++ app/src/main/java/com/gmwapp/dostt/activities/BaseActivity.kt
@@ -15,8 +15,6 @@ import androidx.recyclerview.widget.RecyclerView.SmoothScroller
 import kotlin.math.abs
 
 open class BaseActivity : AppCompatActivity() {
-    private val mShrinkAmount = 0.20f
-    private val mShrinkDistance = 0.9f
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
@@ -27,84 +25,14 @@ open class BaseActivity : AppCompatActivity() {
     }
 
     fun setCenterLayoutManager(recyclerView: RecyclerView){
-        val centerLayoutManager = object : CenterLayoutManager(this, LinearLayoutManager.HORIZONTAL, false) {
-            override fun checkLayoutParams(lp: RecyclerView.LayoutParams): Boolean {
-                lp.width = width / 2
-                lp.height = width / 2
-                return true
-            }
-
-            override fun smoothScrollToPosition(
-                recyclerView: RecyclerView,
-                state: RecyclerView.State,
-                position: Int
-            ) {
-                val smoothScroller: SmoothScroller = CenterSmoothScroller(recyclerView.context)
-                smoothScroller.targetPosition = position
-                startSmoothScroll(smoothScroller)
-            }
-
-            override fun computeScrollVectorForPosition(targetPosition: Int): PointF? {
-                return super.computeScrollVectorForPosition(targetPosition)
-            }
-
-            override fun calculateSpeedPerPixel(displayMetrics: DisplayMetrics): Float {
-                return 5000F / displayMetrics.densityDpi
-            }
-
-            private open inner class CenterSmoothScroller(context: Context?) :
-                LinearSmoothScroller(context) {
-                override fun calculateDtToFit(
-                    viewStart: Int,
-                    viewEnd: Int,
-                    boxStart: Int,
-                    boxEnd: Int,
-                    snapPreference: Int
-                ): Int {
-                    return (boxStart + (boxEnd - boxStart) / 2) - (viewStart + (viewEnd - viewStart) / 2)
-                }
-            }
-
-            override fun canScrollHorizontally(): Boolean {
-                return true
-            }
-
-            override fun scrollHorizontallyBy(
-                dx: Int,
-                recycler: RecyclerView.Recycler?,
-                state: RecyclerView.State?
-            ): Int {
-                val orientation = orientation
-                if (orientation == HORIZONTAL) {
-                    val scrolled = super.scrollHorizontallyBy(dx, recycler, state)
-
-                    val midpoint = width / 2f
-                    val d0 = 0f
-                    val d1 = mShrinkDistance * midpoint
-                    val s0 = 1f
-                    val s1 = 1f - mShrinkAmount
-                    for (i in 0 until childCount) {
-                        val child = getChildAt(i)
-                        val childMidpoint =
-                            (getDecoratedRight(child!!) + getDecoratedLeft(child)) / 2f
-                        val d = d1.coerceAtMost(abs(midpoint - childMidpoint))
-                        val scale = s0 + (s1 - s0) * (d - d0) / (d1 - d0)
-                        child.scaleX = scale
-                        child.scaleY = scale
-                    }
-                    return scrolled
-                } else {
-                    return 0
-                }
-
-            }
-        }
+        val centerLayoutManager = CenterLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         recyclerView.setLayoutManager(centerLayoutManager)
-
     }
 }
 
 open class CenterLayoutManager : LinearLayoutManager {
+    private val mShrinkAmount = 0.20f
+    private val mShrinkDistance = 0.9f
     constructor(context: Context?) : super(context)
 
     constructor(context: Context?, orientation: Int, reverseLayout: Boolean) : super(
@@ -119,6 +47,12 @@ open class CenterLayoutManager : LinearLayoutManager {
         defStyleAttr: Int,
         defStyleRes: Int
     ) : super(context, attrs, defStyleAttr, defStyleRes)
+
+    override fun checkLayoutParams(lp: RecyclerView.LayoutParams): Boolean {
+        lp.width = width / 2
+        lp.height = width / 2
+        return true
+    }
 
     override fun smoothScrollToPosition(
         recyclerView: RecyclerView,
@@ -142,7 +76,37 @@ open class CenterLayoutManager : LinearLayoutManager {
         }
     }
 
-    open fun calculateSpeedPerPixel(displayMetrics: DisplayMetrics): Float {
-        TODO("Not yet implemented")
+    override fun canScrollHorizontally(): Boolean {
+        return true
+    }
+
+    override fun scrollHorizontallyBy(
+        dx: Int,
+        recycler: RecyclerView.Recycler?,
+        state: RecyclerView.State?
+    ): Int {
+        val orientation = orientation
+        if (orientation == HORIZONTAL) {
+            val scrolled = super.scrollHorizontallyBy(dx, recycler, state)
+
+            val midpoint = width / 2f
+            val d0 = 0f
+            val d1 = mShrinkDistance * midpoint
+            val s0 = 1f
+            val s1 = 1f - mShrinkAmount
+            for (i in 0 until childCount) {
+                val child = getChildAt(i)
+                val childMidpoint =
+                    (getDecoratedRight(child!!) + getDecoratedLeft(child)) / 2f
+                val d = d1.coerceAtMost(abs(midpoint - childMidpoint))
+                val scale = s0 + (s1 - s0) * (d - d0) / (d1 - d0)
+                child.scaleX = scale
+                child.scaleY = scale
+            }
+            return scrolled
+        } else {
+            return 0
+        }
+
     }
 }
