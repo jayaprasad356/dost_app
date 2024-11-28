0beec5b2b87f4313eeb2954b76a9ffae542c0446
Sivavishnu0705 <32070669+Sivavishnu0705@users.noreply.github.com>
2024-11-28 16:32:28 +0530

    updated

--- app/src/main/java/com/gmwapp/dostt/fragments/ProfileFragment.kt
+++ app/src/main/java/com/gmwapp/dostt/fragments/ProfileFragment.kt
@@ -1,25 +1,17 @@
 package com.gmwapp.dostt.fragments
 
-import android.R
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.gmwapp.dostt.BaseApplication
 import com.gmwapp.dostt.activities.AccountPrivacyActivity
 import com.gmwapp.dostt.activities.EditProfileActivity
 import com.gmwapp.dostt.activities.TransactionsActivity
-import com.gmwapp.dostt.activities.VerifyOTPActivity
 import com.gmwapp.dostt.activities.WalletActivity
-import com.gmwapp.dostt.constants.DConstants
-import com.gmwapp.dostt.databinding.ActivityMainBinding
-import com.gmwapp.dostt.databinding.FragmentHomeBinding
 import com.gmwapp.dostt.databinding.FragmentProfileBinding
 import com.gmwapp.dostt.dialogs.BottomSheetLogout
-import com.gmwapp.dostt.dialogs.BottomSheetWelcomeBonus
-
 
 class ProfileFragment : BaseFragment() {
     lateinit var binding: FragmentProfileBinding
@@ -31,7 +23,7 @@ class ProfileFragment : BaseFragment() {
     ): View {
         binding = FragmentProfileBinding.inflate(layoutInflater)
 
-        binding.tvName.setText(BaseApplication.getInstance()?.getPrefs()?.getUserData()?.name)
+        binding.tvName.text = BaseApplication.getInstance()?.getPrefs()?.getUserData()?.name
         binding.clWallet.setOnClickListener(View.OnClickListener {
             val intent = Intent(context, WalletActivity::class.java)
             startActivity(intent)
