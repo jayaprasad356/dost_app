f52717cc6bf0250e0b57e671026335c462523b4f
Sivavishnu0705 <32070669+Sivavishnu0705@users.noreply.github.com>
2024-11-30 12:15:21 +0530

    updated

--- app/src/main/java/com/gmwapp/hima/dialogs/BottomSheetDeleteAccount.kt
+++ app/src/main/java/com/gmwapp/hima/dialogs/BottomSheetDeleteAccount.kt
@@ -1,16 +1,19 @@
 package com.gmwapp.hima.dialogs
 
+import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.gmwapp.hima.R
 import com.gmwapp.hima.callbacks.OnButtonClickListener
 import com.gmwapp.hima.callbacks.OnItemSelectionListener
 import com.gmwapp.hima.databinding.BottomSheetDeleteAccountBinding
 import com.gmwapp.hima.databinding.BottomSheetLogoutBinding
 import com.gmwapp.hima.retrofit.responses.Country
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -28,6 +31,9 @@ class BottomSheetDeleteAccount : BottomSheetDialogFragment() {
         initUI()
         return binding.root
     }
+
+    override fun getTheme(): Int = R.style.BottomSheetDialogTheme
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog = BottomSheetDialog(requireContext(), theme)
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
