39d96f1d6f8e4ec7a7e3e03246d64d792e99c8fa
Sivavishnu0705 <32070669+Sivavishnu0705@users.noreply.github.com>
2024-11-30 11:23:41 +0530

    updated

--- app/src/main/java/com/gmwapp/hima/adapters/DeleteReasonAdapter.kt
+++ app/src/main/java/com/gmwapp/hima/adapters/DeleteReasonAdapter.kt
@@ -1,6 +1,7 @@
 package com.gmwapp.hima.adapters
 
 import android.app.Activity
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -31,20 +32,26 @@ class DeleteReasonAdapter(
         val holder: ItemHolder = holderParent as ItemHolder
         val reason: Reason = reasons[position]
 
-        holder.binding.main.setOnClickListener(View.OnClickListener {
+        holder.binding.main.setOnClickListener {
             onItemSelectionListener.onItemSelected(reason)
             if (reason.name == "Other") {
-                isOther = true
+                isOther = reason.isSelected != true
+                reasons.find { it.name != "Other" }?.isSelected = false
             }
             reason.isSelected = reason.isSelected == null || reason.isSelected == false
             reasons[position] = reason
             notifyDataSetChanged()
-        })
+        }
         if (isOther) {
             if (reason.name == "Other") {
-                holder.binding.main.isEnabled = true
-                holder.binding.main.setBackgroundResource(R.drawable.d_button_bg_interest_selected)
-                holder.binding.tvReason.setTextColor(activity.getColor(R.color.black))
+                if (reason.isSelected == true) {
+                    holder.binding.main.isEnabled = true
+                    holder.binding.main.setBackgroundResource(R.drawable.d_button_bg_interest_selected)
+                    holder.binding.tvReason.setTextColor(activity.getColor(R.color.black))
+                } else {
+                    holder.binding.main.setBackgroundResource(R.drawable.d_button_bg_interest)
+                    holder.binding.tvReason.setTextColor(activity.getColor(R.color.interest_text_color))
+                }
             } else {
                 holder.binding.main.isEnabled = false
                 holder.binding.main.setBackgroundResource(R.drawable.d_button_bg_interest_disabled)
