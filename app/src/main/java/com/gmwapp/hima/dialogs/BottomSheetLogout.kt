d1b4be0512e0b944465641de3bad987d8e978a91
Sivavishnu0705 <32070669+Sivavishnu0705@users.noreply.github.com>
2024-12-02 17:03:41 +0530

    updated

--- app/src/main/java/com/gmwapp/hima/dialogs/BottomSheetLogout.kt
+++ app/src/main/java/com/gmwapp/hima/dialogs/BottomSheetLogout.kt
@@ -1,9 +1,12 @@
 package com.gmwapp.hima.dialogs
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.gmwapp.hima.BaseApplication
+import com.gmwapp.hima.activities.LoginActivity
 import com.gmwapp.hima.databinding.BottomSheetLogoutBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
@@ -11,13 +14,20 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 class BottomSheetLogout : BottomSheetDialogFragment() {
     lateinit var binding: BottomSheetLogoutBinding
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = BottomSheetLogoutBinding.inflate(layoutInflater)
 
+        initUI()
         return binding.root
     }
 
+    private fun initUI() {
+        val prefs = BaseApplication.getInstance()?.getPrefs()
+        prefs?.clearUserData()
+        val intent = Intent(context, LoginActivity::class.java)
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+
+    }
 }
