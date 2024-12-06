94dacd4379ed4d55dd25e820525ad1af2a15e328
Sivavishnu0705 <32070669+Sivavishnu0705@users.noreply.github.com>
2024-12-07 05:14:31 +0530

    updated

--- app/src/main/java/com/gmwapp/hima/dialogs/BottomSheetWelcomeBonus.kt
+++ app/src/main/java/com/gmwapp/hima/dialogs/BottomSheetWelcomeBonus.kt
@@ -1,11 +1,14 @@
 package com.gmwapp.hima.dialogs
 
+import android.app.Dialog
 import android.graphics.Paint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.gmwapp.hima.R
 import com.gmwapp.hima.databinding.BottomSheetWelcomeBonusBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 
@@ -21,5 +24,7 @@ class BottomSheetWelcomeBonus : BottomSheetDialogFragment() {
 
         return binding.root
     }
+    override fun getTheme(): Int = R.style.BottomSheetDialogTheme
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog = BottomSheetDialog(requireContext(), theme)
 
 }
