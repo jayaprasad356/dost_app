325d5814c90331ef8e8fd92694d28fbc16fdc722
Sivavishnu0705 <32070669+Sivavishnu0705@users.noreply.github.com>
2024-12-15 09:46:56 +0530

    updated

--- app/src/main/java/com/gmwapp/hima/activities/VerifyOTPActivity.kt
+++ app/src/main/java/com/gmwapp/hima/activities/VerifyOTPActivity.kt
@@ -7,6 +7,7 @@ import android.os.CountDownTimer
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 import com.gmwapp.hima.BaseApplication
@@ -18,7 +19,7 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class VerifyOTPActivity : BaseActivity() {
-    private var timer: CountDownTimer?=null
+    private var timer: CountDownTimer? = null
     lateinit var binding: ActivityVerifyOtpBinding
     private val loginViewModel: LoginViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +28,7 @@ class VerifyOTPActivity : BaseActivity() {
         binding = ActivityVerifyOtpBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initUI()
-        startTimer();
+        startTimer()
     }
 
     private fun initUI() {
@@ -43,7 +44,7 @@ class VerifyOTPActivity : BaseActivity() {
         })
         loginViewModel.sendOTPResponseLiveData.observe(this, Observer {
             binding.pbLoader.visibility = View.GONE
-            binding.btnResendOtp.setText(getString(R.string.resend_otp))
+            binding.btnResendOtp.text = getString(R.string.resend_otp)
             binding.btnResendOtp.visibility = View.GONE
             binding.tvDidntReceivedOtpTimer.visibility = View.VISIBLE
             startTimer()
@@ -51,35 +52,36 @@ class VerifyOTPActivity : BaseActivity() {
 
         loginViewModel.loginErrorLiveData.observe(this, Observer {
             binding.pbVerifyOtpLoader.visibility = View.GONE
-            binding.btnVerifyOtp.setText(getString(R.string.verify_otp))
+            binding.btnVerifyOtp.text = getString(R.string.verify_otp)
             binding.btnVerifyOtp.isEnabled = true
         })
         loginViewModel.loginResponseLiveData.observe(this, Observer {
             binding.pbVerifyOtpLoader.visibility = View.GONE
-            binding.btnVerifyOtp.setText(getString(R.string.verify_otp))
+            binding.btnVerifyOtp.text = getString(R.string.verify_otp)
             binding.btnVerifyOtp.isEnabled = true
             if (it.success) {
                 if (it.registered) {
                     it.data?.let { it1 ->
                         BaseApplication.getInstance()?.getPrefs()?.setUserData(it1)
                     }
-                    var intent:Intent? = null
-                    if(it.data?.gender == DConstants.MALE) {
+                    var intent: Intent? = null
+                    if (it.data?.gender == DConstants.MALE) {
                         intent = Intent(this, MainActivity::class.java)
                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
                         startActivity(intent)
-                    }else{
-                        if(it.data?.status == 2){
+                    } else {
+                        if (it.data?.status == 2) {
                             intent = Intent(this, MainActivity::class.java)
                             intent.putExtra(
-                                DConstants.AVATAR_ID, getIntent().getIntExtra(DConstants.AVATAR_ID, 0)
+                                DConstants.AVATAR_ID,
+                                getIntent().getIntExtra(DConstants.AVATAR_ID, 0)
                             )
-                            intent.putExtra(DConstants.LANGUAGE, it.data?.language)
+                            intent.putExtra(DConstants.LANGUAGE, it.data.language)
                             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                        } else if(it.data?.status == 1){
+                        } else if (it.data?.status == 1) {
                             intent = Intent(this, AlmostDoneActivity::class.java)
                             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                        } else{
+                        } else {
                             intent = Intent(this, VoiceIdentificationActivity::class.java)
                             intent.putExtra(DConstants.LANGUAGE, it.data?.language)
                             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
@@ -96,7 +98,7 @@ class VerifyOTPActivity : BaseActivity() {
         })
 
         binding.btnResendOtp.setOnClickListener({
-            binding.btnResendOtp.setText("")
+            binding.btnResendOtp.text = ""
             binding.pbLoader.visibility = View.VISIBLE
             loginViewModel.sendOTP(mobileNumber, countryCode, otp)
         })
@@ -108,31 +110,35 @@ class VerifyOTPActivity : BaseActivity() {
             }
 
             override fun afterTextChanged(s: Editable) {
-                if(s.toString().length==6){
-                 //   binding.btnVerifyOtp.setBackgroundResource(R.drawable.d_button_bg_white)
+                if (s.toString().length == 6) {
+                    //   binding.btnVerifyOtp.setBackgroundResource(R.drawable.d_button_bg_white)
                     binding.btnVerifyOtp.isEnabled = true
-                }else{
-                //    binding.btnVerifyOtp.setBackgroundResource(R.drawable.d_button_bg)
+                } else {
+                    //    binding.btnVerifyOtp.setBackgroundResource(R.drawable.d_button_bg)
                     binding.btnVerifyOtp.isEnabled = false
                 }
             }
-        }
-        )
+        })
         binding.btnVerifyOtp.setOnClickListener {
             val enteredOTP = binding.pvOtp.text.toString().toInt()
             if (enteredOTP == otp) {
                 binding.pbVerifyOtpLoader.visibility = View.VISIBLE
-                binding.btnVerifyOtp.setText("")
+                binding.btnVerifyOtp.text = ""
                 login(mobileNumber)
+            } else {
+                Toast.makeText(this, getString(R.string.please_enter_the_valid_otp), Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    private fun startTimer(){
+    private fun startTimer() {
         timer = object : CountDownTimer(60000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 val time = millisUntilFinished / 1000
-                binding.tvDidntReceivedOtpTimer.setText(getString(R.string.retry_in, if(time<10) "0$time" else time.toString()))
+                binding.tvDidntReceivedOtpTimer.text = getString(
+                    R.string.retry_in,
+                    if (time < 10) "0$time" else time.toString()
+                )
             }
 
             override fun onFinish() {
