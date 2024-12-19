08623e5bb13367c3699cea9ea5c8f747b5f45585
Sivavishnu0705 <32070669+Sivavishnu0705@users.noreply.github.com>
2024-12-19 22:21:51 +0530

    Update FemaleHomeFragment.kt

--- app/src/main/java/com/gmwapp/hima/fragments/FemaleHomeFragment.kt
+++ app/src/main/java/com/gmwapp/hima/fragments/FemaleHomeFragment.kt
@@ -89,6 +89,8 @@ class FemaleHomeFragment : BaseFragment() {
                     initializeCall()
                 }
             }
+        }else{
+            initializeCall()
         }
     }
 
